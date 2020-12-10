664132cd586de3720531079754e14f64eff4aa79
hyt <1732447369@qq.com>
2020-12-11 00:09:19 +0800

    first

---
+++ src/test/java/com/hyt/ws/personalwebsite/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyt.ws.personalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
